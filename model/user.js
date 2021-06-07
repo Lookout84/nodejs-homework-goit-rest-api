5b3bfcaa3406190904fa7f942f8ab7e08ad088d7
Oleksandr Ivashchuk <thelookout84@gmail.com>
2021-06-07 16:45:54 +0300

    update

---
+++ model/user.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require("mongoose");
+const { isEmail } = require("validator");
+const { Subscription } = require("../helpers/constants");
+const bcrypt = require("bcryptjs");
+
+const SALT_WORK_FACTOR = 8;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      minlength: 3,
+      default: "Guest",
+    },
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      validate: [isEmail, "Please fill a valid email address"],
+    },
+    subscription: {
+      type: String,
+      enum: Object.values(Subscription),
+      default: Subscription.STARTER,
+    },
+    token: { type: String, default: null },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
+});
+
+userSchema.methods.isValidPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = model("user", userSchema);
+
+module.exports = User;
