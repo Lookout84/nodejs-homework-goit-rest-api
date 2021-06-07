ef1b55fd8ee613cc6b8a187de9a13731a1ca999f
Oleksandr Ivashchuk <67271891+Lookout84@users.noreply.github.com>
2021-06-07 18:07:18 +0300

    update users

---
+++ controllers/users.js
@@ -0,0 +1,64 @@
+const Users = require("../repositories/users");
+const { HttpCode } = require("../helpers/constants");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const SECRET_KEY = process.env.SECRET_KEY;
+
+const register = async (req, res, next) => {
+  try {
+    const userEmail = await Users.findByEmail(req.body.email);
+    const userName = await Users.findByName(req.body.name);
+    if (userEmail || userName) {
+      return res.status(HttpCode.CONFLICT).json({
+        status: "error",
+        code: HttpCode.CONFLICT,
+        message: "Email in use",
+      });
+    }
+
+    const { id, name, email, subscription } = await Users.create(req.body);
+
+    return res.status(HttpCode.CREATED).json({
+      status: "success",
+      code: HttpCode.CREATED,
+      user: { id, name, email, subscription },
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+const login = async (req, res, next) => {
+  try {
+    const user = await Users.findByEmail(req.body.email);
+    const isValidPassword = await user?.isValidPassword(req.body.password);
+    if (!user || !isValidPassword) {
+      return res.status(HttpCode.UNAUTHORIZED).json({
+        status: "error",
+        code: HttpCode.UNAUTHORIZED,
+        message: "Email or password is wrong",
+      });
+    }
+    const id = user.id;
+    const payload = { id, test: "test" };
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "2h" });
+    await Users.updateToken(id, token);
+    return res.json({ status: "success", code: 200, data: { token } });
+  } catch (e) {
+    next(e);
+  }
+};  
+
+
+const logout = async (req, res, next) => {
+    try {
+      const id = req.user.id
+      await Users.updateToken(id, null)
+      return res.status(HttpCode.NO_CONTENT).json({})
+    } catch (e) {
+      next(e)
+    }
+  }
+
+
+module.exports = { register, login, logout }
---
+++ routes/api/contacts/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const ctrl = require("../../../controllers/contacts");
+
+const {
+  validationCreateContact,
+  validationUpdateContact,
+  validationUpdateFavoriteStatus,
+  validateContactId,
+} = require("./validation");
+
+router.use((req, res, next) => {
+  console.log(req.url);
+  next();
+});
+
+router
+  .get("/", ctrl.listContacts)
+  .post("/", validationCreateContact, ctrl.addContact);
+
+router
+  .get("/:contactId", validateContactId, ctrl.getContactById)
+  .delete("/:contactId", validateContactId, ctrl.removeContact)
+  .put("/:contactId", validateContactId, validationUpdateContact, ctrl.updateContact);
+
+router.patch(
+  "/:contactId/favorite",
+  validateContactId,
+  validationUpdateFavoriteStatus,
+  ctrl.updateStatusContact
+);
+
+module.exports = router;
